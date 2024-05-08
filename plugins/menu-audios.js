f72c8326b93327c0a7c35e8015f881fd0a02abe9
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-05-08 23:48:42 +0300

    гшен

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -29,60 +29,82 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
-║    ◉— *𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭* —◉
+║    ◉— *Ванилька* —◉
 ║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
-║➤ *𝗛ola, ${taguser}*
+║➤ *Привет, ${taguser}*
 ║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
 ╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
 
 
 
-┏━━━━━━━━━━━━━━━━┓
-┃ *<𝐌𝐄𝐍𝐔 𝐀𝐔𝐃𝐈𝐎𝐒/>*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ *<ღ꧁ *ГОЛОСОВЫЕ* ꧂ღ>*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
 ┣ *- ${_translate.plugins.menu_audios.texto1}*
-┣ ඬ⃟🔊 _Quien es tu sempai botsito 7w7_
-┣ ඬ⃟🔊 _Te diagnostico con gay_
-┣ ඬ⃟🔊 _No digas eso papu_
-┣ ඬ⃟🔊 _A nadie le importa_
-┣ ඬ⃟🔊 _Fiesta del admin_
-┣ ඬ⃟🔊 _Fiesta del administrador_ 
-┣ ඬ⃟🔊 _Vivan los novios_
-┣ ඬ⃟🔊 _Feliz cumpleaños_
-┣ ඬ⃟🔊 _Noche de paz_
-┣ ඬ⃟🔊 _Buenos dias_
-┣ ඬ⃟🔊 _Buenos tardes_
-┣ ඬ⃟🔊 _Buenos noches_
-┣ ඬ⃟🔊 _Audio hentai_
-┣ ඬ⃟🔊 _Chica lgante_
-┣ ඬ⃟🔊 _Feliz navidad_
-┣ ඬ⃟🔊 _Vete a la vrg_
-┣ ඬ⃟🔊 _Pasa pack Bot_
-┣ ඬ⃟🔊 _Atencion grupo_
-┣ ඬ⃟🔊 _Marica quien_
-┣ ඬ⃟🔊 _Murio el grupo_
-┣ ඬ⃟🔊 _Oh me vengo_
-┣ ඬ⃟🔊 _tio que rico_
-┣ ඬ⃟🔊 _Viernes_
-┣ ඬ⃟🔊 _Baneado_
-┣ ඬ⃟🔊 _Sexo_
-┣ ඬ⃟🔊 _Hola_
-┣ ඬ⃟🔊 _Un pato_
-┣ ඬ⃟🔊 _Nyanpasu_
-┣ ඬ⃟🔊 _Te amo_
-┣ ඬ⃟🔊 _Yamete_
-┣ ඬ⃟🔊 _Bañate_
-┣ ඬ⃟🔊 _Es puto_
-┣ ඬ⃟🔊 _La biblia_
-┣ ඬ⃟🔊 _Onichan_
-┣ ඬ⃟🔊 _Mierda de Bot_
-┣ ඬ⃟🔊 _Siuuu_
-┣ ඬ⃟🔊 _Epico_
-┣ ඬ⃟🔊 _Shitpost_
-┣ ඬ⃟🔊 _Rawr_
-┣ ඬ⃟🔊 _UwU_
-┣ ඬ⃟🔊 _:c_
-┣ ඬ⃟🔊 _a_
+┣ >>>>>>>>>>>>>>>>>>>>>>
+┣ 🔊 Красавчик
+┣ 🔊 Где все
+┣ 🔊 Что случилось
+┣ 🔊 Дай
+┣ 🔊 Группа
+┣ 🔊 Как дела
+┣ 🔊 Что нового
+┣ 🔊 Что делаешь
+┣ 🔊 Пока
+┣ 🔊 До свидания
+┣ 🔊 Псих
+┣ 🔊 Идиот
+┣ 🔊 Совет
+┣ 🔊 Жопа
+┣ 🔊 Как ты
+┣ 🔊 Дай
+┣ 🔊 Друг
+┣ 🔊 Как жизнь
+┣ 🔊 Девушка/и
+┣ 🔊 Чего
+┣ 🔊 Ух какая
+┣ 🔊 Страшная
+┣ 🔊 Мечта
+┣ 🔊 Молчуны
+┣ 🔊 Цель
+┣ 🔊 Слышь ты
+┣ 🔊 Кушаю
+┣ 🔊 От души
+┣ 🔊 😘
+┣ 🔊 Надоел
+┣ 🔊 Отвали
+┣ 🔊 Нет настроения
+┣ 🔊 Молодец
+┣ 🔊 Споки ноки 
+┣ 🔊 Заебал
+┣ 🔊 Врун
+┣ 🔊 Пиздабол
+┣ 🔊 Правила
+┣ 🔊 Здарова
+┣ 🔊 Кто ты
+┣ 🔊 Сука
+┣ 🔊 Секс
+┣ 🔊 Пошел ты
+┣ 🔊 Хи хи
+┣ 🔊 Помогите
+┣ 🔊 До завтра
+┣ 🔊 Красотка
+┣ 🔊 Блять
+┣ 🔊 Тупой/ая
+┣ 🔊 Ты кто
+┣ 🔊 Удали/ю
+┣ 🔊 Понедельник
+┣ 🔊 😡
+┣ 🔊 😭
+┣ 🔊 Кто звал
+┣ 🔊 Пришел
+┣ 🔊 Я тут
+┣ 🔊 Дурак
+┣ 🔊 Козел
+┣ 🔊 Лох
+┣ 🔊 Сиськи
+┣ 🔊 Сюрприз
+┣ 🔊 Чмо
 ┗━━━━━━━━━━━━━━━━┛`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
@@ -97,7 +119,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     conn.reply(m.chat, tradutor.texto2, m);
   }
 };
-handler.command = /^(цыкеорцкеор|audios|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|keyaudio|keyaudios)$/i;
+handler.command = /^(меню2|audios|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|keyaudio|keyaudios)$/i;
 handler.exp = 50;
 handler.fail = null;
 export default handler;
